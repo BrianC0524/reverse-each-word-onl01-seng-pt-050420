e9cb9bed6b3d4aa3d938f0508df047dd35253f79
Brian Craycraft <craycraftb24@hotmail.com>
2020-05-15 02:37:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,6 +6,6 @@ def reverse_each_word(sentence)
   
   string_to_array = "#{sentence}".split
   
-  string_to_array.each {|letter| puts "#{letter}".reverse }.join()
+  string_to_array.each {|letter| puts "#{letter}".reverse }.join("")
   string_to_array
 end
